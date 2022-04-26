44e07cd34b9064c4a83decb2e52f9fe5147fa8e9
dkumar <dkumar@fortinet.com>
2022-04-26 22:48:53 +0530

    Added base64 text

--- decoding_text.py
+++ decoding_text.py
@@ -1,6 +1,6 @@
 import base64
 
-base64_message = 'UHl0aG9uIGlzIGZ1bg=='
+base64_message = 'SG9va2VkIG9uIHBob25pY3Mgd29ya2VkIGZvciBtZSBhbmQgeW91Cg=='
 base64_bytes = base64_message.encode('ascii')
 message_bytes = base64.b64decode(base64_bytes)
 message = message_bytes.decode('ascii')
